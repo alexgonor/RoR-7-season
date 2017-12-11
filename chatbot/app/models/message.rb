ec02adc8af670ed2781a4a7934d491beeda66791
alexgonor <gonor.alexandr@gmail.com>
2017-12-11 11:10:36 +0200

    remove trash

--- chatbot/app/models/message.rb
+++
@@ -1,4 +0,0 @@
-class Message < ApplicationRecord
-  belongs_to :conversation
-  validates_inclusion_of :kind, :in => ["outgoing", "incoming"], allow_nil: false
-end
--- chatbot/config/routes.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.routes.draw do
-  post '/start', to: 'chat#start'
-  post '/message', to: 'chat#message'
-end
